f7d94be534eb6d7d59663d68dee67d8134248a1c
Alex Barnett <aslbarnett@gmail.com>
2023-01-02 18:11:14 +0000

    👌 IMPROVE: Update theme generation

---
+++ src/themes.ts
@@ -0,0 +1,31 @@
+import { BaseVariant, Shade, Theme, Variant } from './types';
+
+const themeMap = [
+  { label: 'lapis', color: 'lapis' },
+  { label: 'lapis-amethyst', color: 'amethyst' },
+  { label: 'lapis-ruby', color: 'ruby' },
+  { label: 'lapis-amber', color: 'amber' },
+  { label: 'lapis-peridot', color: 'peridot' },
+  { label: 'lapis-aquamarine', color: 'aquamarine' },
+  { label: 'lapis-quartz', alias: 'lapis-onyx', color: 'quartz' },
+];
+
+const variants = ['default', 'stealth', 'dark', 'dark-stealth', 'light'];
+
+export const themes: Array<Theme> = [
+  ...themeMap
+    .map(({ label, color, alias }) => {
+      return variants.map((variant) => ({
+        name: `${variant === 'light' && color === 'quartz' ? alias : label}${
+          variant === 'default' ? '' : `-${variant}`
+        }`,
+        baseVariant: variant.includes('stealth')
+          ? ('stealth' as BaseVariant)
+          : ('default' as BaseVariant),
+        variant: color as Variant,
+        ...(variant.includes('dark') && { shade: 'dark' as Shade }),
+        ...(variant.includes('light') && { shade: 'light' as Shade }),
+      }));
+    })
+    .reduce((acc, curr) => [...acc, ...curr], []),
+];
